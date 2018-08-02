326c03c1450d1063ccd63ff9ddbbfa45c369563f
Andras Noszka <andras.noszka@windowslive.com>
2018-08-02 15:35:17 +0200

    (Part 1) : Basic String and Math exercises

---
+++ week-01/day-4/workshop_part1.ts
@@ -0,0 +1,42 @@
+console.log('Humpty Dumpty sat on a wall,');
+console.log('Humpty Dumpty had a great fall.');
+console.log('All the king\'s horses and all the king\'s men');
+console.log('Couldn\'t put Humpty together again.');
+
+console.log('Hello Kamilla');
+console.log('Hello Eszter');
+console.log('Hello Mark');
+
+console.log('Andras Noszka');
+console.log('30');
+console.log(1.80);
+
+let x = 22;
+let y = 13;
+console.log(x + y);
+console.log(y - x);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+let semester = 30;
+let workhours = 52;
+console.log(semester / workhours * 100);
+
+let favoriteNumber: number = 8;
+console.log("My favorite number " + favoriteNumber);
+
+let a: number = 123;
+let b: number = 526;
+let c: number = a;
+ a = b;
+ b = c;
+ console.log(a);
+ console.log(b);
+
+ let massInKg: number = 81.2;
+ let heightInm: number = 1.78;
+ let BMI = massInKg / (heightInm * heightInm);
+ console.log("My BMI is " + BMI);
+
+
