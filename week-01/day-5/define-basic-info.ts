7d39e135134c64f7f924be0ce82c5d15dd2e8972
Andras Noszka <andras.noszka@windowslive.com>
2018-08-04 19:00:20 +0200

    Completed: Define-basic-info.ts

---
+++ week-01/day-5/define-basic-info.ts
@@ -0,0 +1,6 @@
+let names: string = 'Andras Noszka';
+let age: number = 30;
+let height: number = 1.80;
+let married: boolean = false;
+
+console.log('Name:' + names + ' ' + 'Age:'+ age + ' ' + 'Height:' + height + 'm' + ' ' + 'Married:' + married);
