227f3a9002d2c36601443b796b81dc86fca94c92
Andras Noszka <andras.noszka@windowslive.com>
2018-08-04 19:35:26 +0200

    Completed: Variable-mutations.ts

---
+++ week-01/day-5/varialbe-mutations.ts
@@ -0,0 +1,46 @@
+let a: number = 3;
+a = a + 10;
+console.log(a);
+
+let b: number = 100;
+b = b - 7;
+console.log(b);
+
+let c: number = 44;
+c = c * 2;
+console.log(c);
+
+let d: number = 125;
+d = 125 / 5;
+console.log(d);
+
+let e: number = 8;
+e = e * e * e;
+console.log(e);
+
+let f1: number = 123;
+let f2: number = 345;
+let result: boolean = f1 > f2;
+console.log(result);
+
+let g1: number = 350;
+let g2: number = 200;
+console.log((g2 *g2) > g1);
+
+let h: number = 1357988018575474;
+console.log(h % 11 == 0);
+
+let i1: number = 10;
+let i2: number = 3;
+console.log(i1 > (i2 * i2) && i1 < (i2 * i2 * i2) );
+
+let j: number = 1521;
+console.log(j % 3 == 0 || j % 5 == 0);
+
+let k: string = 'Apple';
+console.log(k + ' ' + k + ' ' + k + ' ' + k);
+
+
+
+
+
