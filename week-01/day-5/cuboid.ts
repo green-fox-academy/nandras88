aef56a6e280f698a76e5f76401fc2e3b6be3446c
Andras Noszka <andras.noszka@windowslive.com>
2018-08-04 19:58:29 +0200

    Completed: Cuboid

---
+++ week-01/day-5/cuboid.ts
@@ -0,0 +1,6 @@
+let cuboidSides: number = 10;
+let cuboidSurfaceArea: number = 2 * (cuboidSides * cuboidSides) + 2 * (cuboidSides * cuboidSides) + 2 * (cuboidSides * cuboidSides);
+let cuboidVolume: number = cuboidSides * cuboidSides * cuboidSides;
+
+console.log('Surface Area: ' + cuboidSurfaceArea);
+console.log('Volume: ' + cuboidVolume);
