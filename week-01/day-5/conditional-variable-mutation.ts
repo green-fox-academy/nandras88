5225642b705f33b4aa13e54be6439fe8e526043a
Andras Noszka <andras.noszka@windowslive.com>
2018-08-04 23:37:08 +0200

    Completed: Conditional-Variable-Mutation

---
+++ week-01/day-5/conditional-variable-mutation.ts
@@ -0,0 +1,45 @@
+let a: number = 24;
+let out: number = 0;
+
+if (a % 2 == 0) {
+    out = out + 1;
+    console.log(out);
+}
+
+let b: number = 13;
+let out2: string = '';
+
+if (b >= 10 && b <= 20) {
+    out2 = 'Sweet';
+} else if (b > 10) {
+    out2 = 'Less';
+} else if (b > 20) {
+    out2 = 'More';
+}
+console.log(out2);
+
+let c: number = 123;
+let credits: number = 100;
+let isBonus: boolean = false;
+
+if (credits >= 50 && isBonus == false) {
+    c = c - 2;
+} else if (credits < 50 && isBonus == false) {
+    c = c - 1;
+} else if (isBonus !== false) {
+    c = c;
+}
+console.log(c);
+
+let d: number = 8;
+let time: number = 120;
+let out3: string = '';
+
+if (d % 4 == 0 && time < 200){
+    out3 = 'check';
+} else if ( time < 200){
+    out3 = 'Time out';
+} else {
+    out3 = 'Run Forest Run!';
+}
+console.log(out3);
