f3d8b396682e5b61a26fa76d379d1b9b34482a13
Andras Noszka <andras.noszka@windowslive.com>
2018-08-05 12:07:01 +0200

    Completed: Draw-triangle

---
+++ week-01/day-5/draw-triangle.ts
@@ -0,0 +1,9 @@
+let lineCount: number = 4;
+let rowCount: number = 3;
+let star: string = '*';
+
+console.log(star);
+for (let i: number = 1; i < lineCount; i++){
+       console.log (star += '*');
+       
+}
