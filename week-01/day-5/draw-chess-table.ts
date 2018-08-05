4e1d5ff644231af576ac6336e4a1d366919bc612
Andras Noszka <andras.noszka@windowslive.com>
2018-08-05 16:00:11 +0200

    Completed: Draw-chess-table

---
+++ week-01/day-5/draw-chess-table.ts
@@ -0,0 +1,7 @@
+let lineCount: number = 4;
+let chessTable: string = '%%%%';
+
+for (let i: number = 0; i < lineCount; i++){
+       console.log(chessTable);
+       console.log(' ' + chessTable);
+       }
