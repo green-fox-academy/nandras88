ae16af99f7fc5ece5d592d9f1f9831a635ef8c39
Andras Noszka <andras.noszka@windowslive.com>
2018-08-06 13:43:18 +0200

    Completed: Doubling

---
+++ week-02/day-1/doubling.ts
@@ -0,0 +1,11 @@
+// -  Create a variable named `baseNum` and assign the value `123` to it
+// -  Create a function called `doubling` that doubles it's input parameter and returns with an integer
+// -  Log the result of `doubling(baseNum)` to the console
+
+let baseNum = 123;
+
+function doubling(num : number){
+    return num * 2;
+   }
+
+console.log(doubling(baseNum));
