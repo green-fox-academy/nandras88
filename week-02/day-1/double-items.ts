1bdf0296371cdc5e76cbc3ae706c68f919dfb881
Andras Noszka <andras.noszka@windowslive.com>
2018-08-06 19:10:12 +0200

    Completed: Double-items

---
+++ week-02/day-1/double-items.ts
@@ -0,0 +1,9 @@
+// -  Create an array variable named `drinks` with the following content: `['Gin', 'Whiskey', 'Wine', 'Beer']`
+// -  Double all the strings in the array, use a built in array method instead of a loop
+// It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
+
+let drinks = ['Gin', 'Whiskey', 'Wine', 'Beer'];
+
+drinks = drinks.map( i => i + i);
+
+console.log(drinks);
