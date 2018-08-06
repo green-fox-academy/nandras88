65b6c0644c8fe05e9bf9b96282856a5ff6acb5b0
Andras Noszka <andras.noszka@windowslive.com>
2018-08-06 14:16:43 +0200

    Completed: Append-a-func.ts

---
+++ week-02/day-1/append-a-func.ts
@@ -0,0 +1,13 @@
+// -  Create a string variable named `typo` and assign the value `Chinchill` to it
+// -  Write a function called `appendAFunc` that gets a string as an input,
+//    appends an 'a' character to its end and returns with a string
+// -  Print the result of `appendAFunc(typo)`
+
+let typo: string = 'Chinchill';
+
+function appendAFunc( inputString: string) {
+     return inputString + 'a';
+    
+}
+let resultStrign = appendAFunc(typo);
+console.log(resultStrign);
