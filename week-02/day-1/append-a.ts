5ac17d47b4fe814ce3c7777c76ef8bdf93dd7c7b
Andras Noszka <andras.noszka@windowslive.com>
2018-08-06 17:47:13 +0200

    Completed: Append-a

---
+++ week-02/day-1/append-a.ts
@@ -0,0 +1,8 @@
+// -  Create an array variable named `animals`
+//    with the following content: `["koal", "pand", "zebr"]`
+// -  Add all elements an `"a"` at the end
+
+let animals = ['koal', 'pand', 'zebr'];
+
+animals = animals.map(i => i + 'a');
+console.log(animals);
