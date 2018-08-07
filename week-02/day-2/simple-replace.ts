5a83d78ff7cff27287de79f79f6e23556791392b
Andras Noszka <andras.noszka@windowslive.com>
2018-08-07 11:51:10 +0200

    Completed: Simple-replace

---
+++ week-02/day-2/simple-replace.ts
@@ -0,0 +1,9 @@
+// I would like to replace "dishwasher" with "galaxy" in this example
+// Please fix it for me!
+// Expected ouput: In a galaxy far far away
+
+let example: string = 'In a dishwasher far far away';
+example = example.replace('dishwasher', 'Galaxy');
+console.log(example);
+
+export = example;
