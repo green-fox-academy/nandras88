e941ca6f6a0e4b6c3313b3436cda574e1ebc9f42
Andras Noszka <andras.noszka@windowslive.com>
2018-08-08 19:37:02 +0200

    Completed: Purple steps

---
+++ week-02/day-3/purple-steps/purple-steps-ts.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+let sideOfRectangle = 15;
+let x = 10;
+let y = 10;
+
+function drawPurpleSteps(){
+for (let i: number = 0; i < 19; i++) {
+  ctx.strokeStyle = 'black';
+  ctx.lineWidth = 5;
+  ctx.strokeRect(x, y, sideOfRectangle, sideOfRectangle);
+  ctx.fillStyle = 'purple';
+  ctx.fillRect(x, y, sideOfRectangle , sideOfRectangle);
+  x += sideOfRectangle +5;
+  y += sideOfRectangle +5;
+}
+}
+
+drawPurpleSteps();
