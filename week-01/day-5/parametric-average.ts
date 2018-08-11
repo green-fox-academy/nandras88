bc14b5e589ca3b92b80647a20eb9928726df55c2
Andras Noszka <andras.noszka@windowslive.com>
2018-08-11 10:15:16 +0200

    Completed: Parametic-avarage

---
+++ week-01/day-5/parametric-average.ts
@@ -0,0 +1,13 @@
+
+function parameticAverage(number1, number2, number3) {
+
+  let sum: number;
+  let avarage: number;
+
+  sum = number1 + number2 + number3;
+  avarage = (number1 + number2 + number3) / 3;
+
+  console.log('Sum: ' + sum + ',' + ' Avarage: ' + avarage);
+}
+
+parameticAverage(11, 6, 7);
