e3488d4c8869b3697bfde88b69965ca31b7e8d15
Andras Noszka <andras.noszka@windowslive.com>
2018-08-11 10:55:48 +0200

    Completed: Reverse

--- week-02/day-1/reverse.ts
+++ week-02/day-1/reverse.ts
@@ -1,3 +1,18 @@
 let numList = [3, 4, 5, 6, 7, ];
 console.log(numList.reverse());
 
+
+
+let numListNew = [3 ,4 ,5 ,6 ,7];
+
+function reverseNumlist(){
+  let newNumlist: any = [];
+  for (let i: number =  numListNew.length - 1; i >= 0; i--){
+    newNumlist += numListNew[i];
+
+  }
+  console.log(newNumlist);
+}
+
+reverseNumlist();
+
