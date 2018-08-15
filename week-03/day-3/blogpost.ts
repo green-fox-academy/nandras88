984e4ccc00f16f3e2f9e58746ac43535f0648430
Andras Noszka <andras.noszka@windowslive.com>
2018-08-15 14:21:10 +0200

    Completed: Blog-post

---
+++ week-03/day-3/blogpost.ts
@@ -0,0 +1,20 @@
+class BlogPost {
+
+  authorName: string;
+  title: string;
+  text: string;
+  publicationDate: string;
+
+  constructor(title, authorName, publicationDate, text) {
+    this.title = title;
+    this.authorName = authorName;
+    this.publicationDate = publicationDate;
+    this.text = text;
+  }
+
+}
+
+let loremBlog: BlogPost = new BlogPost('Lorem Ipsum', 'John Doe', '2000.05.04', 'Lorem ipsum dolor sit amet.');
+let waitBlog: BlogPost = new BlogPost('Wait but why', 'Tim Urban', '2010.10.10', 'A popular long-form, stick - figure - illustrated blog about almost everything.');
+let oneBlog: BlogPost = new BlogPost('One Engineer Is Trying to Get IBM to Reckon With Trump', 'William Turton', '2017.03.28.', 'Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.')
+
