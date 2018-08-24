c977d3ae734e7060409d100a7b6c1009cf4510fe
Andras Noszka <andras.noszka@windowslive.com>
2018-08-24 09:04:47 +0200

    fix():Refactoring

--- week-04/day-4/anagram.ts
+++ week-04/day-4/anagram.ts
@@ -3,11 +3,11 @@
 let characters: any[] = [' ', '!', '.', ',', "'", '"', '?']
 
 export function anagram(inputString: string, inputString2: string): boolean {
-  return (inputString.toLowerCase().split('').sort().filter(element => characters.indexOf(element) == -1).join() ==
-    inputString2.toLowerCase().split('').sort().filter(element => characters.indexOf(element) == -1).join()) ;
-  
-  
+  return (checkAnagram(inputString) == checkAnagram(inputString2));
 }
 
 console.log(anagram('was', 'saw'));
 
+function checkAnagram(param:string) {
+  param.toLowerCase().split('').sort().filter(element => characters.indexOf(element) == -1).join()
+}
