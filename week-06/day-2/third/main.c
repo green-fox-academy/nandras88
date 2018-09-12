64a9d26cbf687771fac7ab8cda92054be0fff110
Andras Noszka <andras.noszka@windowslive.com>
2018-09-12 12:56:34 +0200

    Completed: Third

---
+++ week-06/day-2/third/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// - Create an array variable named `q`
+//   with the following content: `[4, 5, 6, 7]`
+// - Print the third element of `q`
+
+int main()
+{
+	int q[] = {4, 5, 6, 7};
+	printf("%d\n", q[2]);
+	return 0;
+}
