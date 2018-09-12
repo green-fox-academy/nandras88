2437c5d6103158645b884ca06e81a3577e420fda
Andras Noszka <andras.noszka@windowslive.com>
2018-09-12 15:50:14 +0200

    Completed: Greet

---
+++ week-06/day-2/greet/main.c
@@ -0,0 +1,15 @@
+#include "greet.h"
+
+// - Create a char array variable named `al` and assign the value `Greenfox` to it
+// - Create a function called `greet` that greets it's input parameter
+//     - Greeting is printing e.g. `Greetings dear, Greenfox`
+// - Greet `al`
+
+int main()
+{
+  char al[] = "Greenfox";
+
+  greeting_function(al);
+
+  return 0;
+}
