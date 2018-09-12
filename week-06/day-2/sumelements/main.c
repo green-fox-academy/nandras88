97debbe782107570ee574ef593dd7c8d738966e1
Andras Noszka <andras.noszka@windowslive.com>
2018-09-12 12:54:39 +0200

    Completed: Sum Elements

---
+++ week-06/day-2/sumelements/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// - Create an array variable named `r`
+//   with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
+
+int main()
+{
+
+	return 0;
+}
