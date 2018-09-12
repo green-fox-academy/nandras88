2e7145535c1c0db003d355b369c18ab289d5794d
Andras Noszka <andras.noszka@windowslive.com>
2018-09-12 13:01:31 +0200

    Completed: Update

---
+++ week-06/day-2/toupdate/toupdate.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// update the value of number variable to 42 using the "number_pointer"
+
+int main()
+{
+	int number = 1234;
+	int *number_pointer = &number;
+	*number_pointer = 42;
+	printf("The updated value : %d ", number);
+	return 0;
+}
