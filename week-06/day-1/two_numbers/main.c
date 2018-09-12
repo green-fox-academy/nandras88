90541085ece3be69dc5e248fa0d0c037d714c80f
Andras Noszka <andras.noszka@windowslive.com>
2018-09-12 11:28:14 +0200

    Completed: Two Numbers

---
+++ week-06/day-1/two_numbers/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+main()
+{
+    printf("%d\n", 13 + 22);
+    printf("%d\n", 9 - 7);
+    printf("%d\n", 2 * 5);
+    printf("%d\n", 6 / 2);
+    printf("%d\n", 7 % 2);
+    printf("%.2f\n", 7 / 2.0);
+}
