12e0db4eee7271bed85c759a8b20635d0599bb84
Andras Noszka <andras.noszka@windowslive.com>
2018-09-13 19:12:34 +0200

    Completed: Position

---
+++ week-06/day-3/position/main.c
@@ -0,0 +1,14 @@
+#include "position.h"
+
+// create a function which takes a char array as a parameter and
+// lists all position where character 'i' is found
+
+int main()
+{
+  char string[55] = "This is a string for testing";
+  char *p;
+
+  listAllIndex(string);
+
+  return 0;
+}
