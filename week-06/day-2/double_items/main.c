c7ecd1b7332077e8c1d53921bc53e63b1a94d639
Andras Noszka <andras.noszka@windowslive.com>
2018-09-14 14:26:03 +0200

    Completed: Double items

---
+++ week-06/day-2/double_items/main.c
@@ -0,0 +1,19 @@
+#include "double.h"
+
+// - Create an array variable named `numList`
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Create a function that takes an array as a parameter and,
+//   double all the values in the original array
+// - Example:
+//   int array[] = {0, 32, 5, 71, 91};
+//   double_items(array);
+//   printf("%d\n", array[1]); // should print out 64
+
+int main()
+{
+  int array[] = {0, 32, 5, 71, 91};
+
+  doubleItem(array);
+
+  return 0;
+}
