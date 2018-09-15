1d287e04afbd81b4058acd21608d644b646f738a
Andras Noszka <andras.noszka@windowslive.com>
2018-09-15 13:51:24 +0200

    Completed:FizzBuzz

---
+++ week-06/day-1/fizzBuzz/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// Write a program that prints the numbers from 1 to 100.
+// But for multiples of three print “Fizz” instead of the number
+// and for the multiples of five print “Buzz”.
+// For numbers which are multiples of both three and five print “FizzBuzz”.
+
+int main()
+{
+  for (int i = 0; i < 100; i++)
+  {
+    if (i % 5 == 0 && i % 3 == 0)
+    {
+      printf("FizzBuzz\n");
+    }
+    else if (i % 3 == 0)
+    {
+      printf("Fizz\n");
+    }
+    else if (i % 5 == 0)
+    {
+      printf("Buzz\n");
+    }
+    else
+    {
+      printf("%d\n", i);
+    }
+  }
+
+  return 0;
+}
