1048b127dee50b54e475a51153887824477d6698
Andras Noszka <andras.noszka@windowslive.com>
2018-09-15 11:48:48 +0200

    feat():function is created

---
+++ week-06/day-1/animals_and_legs/animal.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void legsCalculation(int numberOfChicken, int numberOfPigs)
+{
+  printf("The legs number is the following: %d\n", (numberOfChicken * 2) + (numberOfPigs * 4));
+}
