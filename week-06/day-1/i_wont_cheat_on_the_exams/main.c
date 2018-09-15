6d472a32e5bd28e9ba8faeefbfb3996abdf36d32
Andras Noszka <andras.noszka@windowslive.com>
2018-09-15 12:55:34 +0200

    Completed: I wont cheat on the exams

---
+++ week-06/day-1/i_wont_cheat_on_the_exams/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+int main()
+{
+  for (int i = 0; i < 100; i++)
+  {
+    printf("I won't cheat on the exam!\n");
+  }
+  return 0;
+}
