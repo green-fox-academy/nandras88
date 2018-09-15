fc996d781076de4ed4b394e27fb7cd7b0a3868fd
Andras Noszka <andras.noszka@windowslive.com>
2018-09-15 13:28:45 +0200

    feat():Function created

---
+++ week-06/day-1/count_from_to/count.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+// Create a program that asks for two numbers
+// If the second number is not bigger than the first one it should print:
+// "The second number should be bigger"
+//
+// If it is bigger it should count from the first number to the second by one
+//
+// example:
+//
+// first number: 3, second number: 6, should print:
+//
+// 3
+// 4
+// 5
+
+void count(int firstNumber, int secondNumber)
+{
+  if (firstNumber > secondNumber)
+  {
+    printf("The second number should be bigger\n");
+  }
+  else
+  {
+    for (int i = firstNumber; i < secondNumber; i++)
+    {
+      printf("%d\n", i);
+    }
+  }
+}
