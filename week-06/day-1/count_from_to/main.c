d8b64d88d9873a3aebd19e0c6cc81c565fa4ec16
Andras Noszka <andras.noszka@windowslive.com>
2018-09-15 13:29:00 +0200

    Completed:Count from to

---
+++ week-06/day-1/count_from_to/main.c
@@ -0,0 +1,26 @@
+#include "count.h"
+
+// Create a program that asks for two numbers
+// If the second number is not bigger than the first one it should print:
+// "The second number should be bigger"
+//
+// If it is bigger it should count from the first number to the second by one
+//
+// example:
+//
+// first number: 3, second number: 6, should print:
+//
+// 3
+// 4
+// 5
+
+int main()
+{
+  int a;
+  int b;
+  printf("Please add me two numbers\n");
+  scanf("%d %d", &a, &b);
+  count(a, b);
+
+  return 0;
+}
