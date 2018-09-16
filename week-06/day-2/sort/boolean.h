1db2f58525eb30aadbd630abcaa25fa9dc56ea09
Andras Noszka <andras.noszka@windowslive.com>
2018-09-16 20:43:52 +0200

    feat():Boolean function created

---
+++ week-06/day-2/sort/boolean.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int isItBoolean(int *inputArray, int sizeOfArray)
+{
+    int right = 0;
+    int notRight = 0;
+    int boolean = 0;
+
+    for (int i = 0; i < sizeOfArray; i++)
+    {
+        if (inputArray[i] < inputArray[i + 1])
+            right++;
+        if (inputArray[i] > inputArray[i + 1])
+            notRight++;
+    }
+
+    if (right > 0 && notRight == 1)
+        boolean += 1;
+
+    return boolean;
+}
