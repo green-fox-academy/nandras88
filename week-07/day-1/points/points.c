96dba489f12d42ee077e65c25157cdb6f9a5cdb6
Andras Noszka <andras.noszka@windowslive.com>
2018-09-17 19:29:32 +0200

    feat():Function is created feat():Points is creating

---
+++ week-07/day-1/points/points.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*
+Create a point struct which has two float variables: x and y
+Create a function that constructs a point
+It should take it's x and y coordinate as parameter
+Create a function that takes 2 points and returns the distance between them
+Example:
+int main()
+{
+	point_t p1 = create_point(1, 1);
+	point_t p2 = create_point(1, 5.5);
+	float dist = distance(p1, p2);
+	printf("%f", dist);
+	return 0;
+}
+*/
+
+typedef struct
+{
+    float x;
+    float y;
+
+} Point;
+
+Point create_point(float a, float b);
+
+int main()
+{
+    Point p1 = create_point(1, 1);
+    Point p2 = create_point(1, 5.5);
+    printf("%.1f", p2.y);
+
+    return 0;
+}
+Point create_point(float a, float b)
+{
+    Point p1;
+    p1.x = a;
+    p1.y = b;
+    return p1;
+}
