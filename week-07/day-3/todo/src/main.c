bc807606c3ad6ff5160f0a97823c2e90e11e1966
Andras Noszka <andras.noszka@windowslive.com>
2018-09-19 21:10:54 +0200

    feat():switch case redifine

--- week-07/day-3/todo/src/main.c
+++ week-07/day-3/todo/src/main.c
@@ -6,29 +6,35 @@ int main()
 {
 	todo_t listOfTodo[20];
 
+	char *instruction = "Please give me an Instruction";
+
 	getBasicScreen();
 
+	printf("%s\n", instruction);
 	int exitStatement = 0;
 	while (exitStatement != 1)
 	{
 
 		char introduction;
-		printf("Please give me an Instruction\n");
 		scanf("%c", &introduction);
 
 		switch (introduction)
 		{
 		case 'l':
 			listTasks(listOfTodo);
+			printf("%s\n", instruction);
 			break;
 		case 'a':
 			addTask(listOfTodo);
+			printf("%s\n", instruction);
 			break;
 		case 'r':
 			removeTask(listOfTodo);
+			printf("%s\n", instruction);
 			break;
 		case 'c':
 			completedTask(listOfTodo);
+			printf("%s\n", instruction);
 			break;
 		case 'q':
 			printf("The program is done\n");
