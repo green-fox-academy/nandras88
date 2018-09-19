d154a9947507e6d7768ae5fbc2c2e8eeff265713
Andras Noszka <andras.noszka@windowslive.com>
2018-09-19 16:24:17 +0200

    feat():while loop with exit statement created, switch case placed

---
+++ week-07/day-3/todo/src/main.c
@@ -0,0 +1,41 @@
+#include "todo.h"
+
+int counter;
+
+int main()
+{
+
+	todo_t listOfTodo[20];
+
+	getBasicScreen();
+
+	int exitStatement = 0;
+	while (exitStatement != 1)
+	{
+
+		char introduction;
+		printf("Please give me an Instruction\n");
+		scanf("%c", &introduction);
+
+		switch (introduction)
+		{
+		case 'l':
+			listTasks(listOfTodo);
+			break;
+		case 'a':
+			addTask(listOfTodo);
+			break;
+		case 'r':
+			removeTask(listOfTodo);
+			break;
+		case 'c':
+			completedTask(listOfTodo);
+			break;
+		case 'q':
+			printf("The program is done\n");
+			exitStatement++;
+			break;
+		}
+	}
+	return 0;
+}
